921311b90190f4f660e4fcef1d624af185787d47
Ruben Danielyan <ruben.danielyan@disqo.com>
2022-03-12 01:03:00 +0400

    Update API deployment

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,10 +2,10 @@ import express from "express";
 const app = express();
 
 // Constants
-const PORT: number = 80;
+const PORT: number = 8080;
 
 // define a route handler for the default home page
-app.get( "/", ( req, res ) => {
+app.get( "/", ( req: any, res: any ) => {
     res.send( "hi" );
 } );
 
