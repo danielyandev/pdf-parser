0c345fba958092ac4a9b2d170e7c13efef7e3b40
Ruben Danielyan <ruben.danielyan@disqo.com>
2022-03-13 00:10:07 +0400

    Make user icon a dropdown

--- ui/src/components/header/Header.tsx
+++ ui/src/components/header/Header.tsx
@@ -7,8 +7,20 @@ function Header() {
       <img src="/assets/images/logo.png" alt="logo" className="mx-3 mt-2"/>
 
       <div className="d-flex align-items-center mr-5">
-        <i className="fa fa-user-circle fs-1 color-primary"/>
-        <span className="mx-3">Name Surname</span>
+
+        <div className="dropdown text-end">
+          <a href="#" className="d-block link-dark text-decoration-none d-flex align-items-center"
+             id="account-dropdown"
+             data-bs-toggle="dropdown"
+             aria-expanded="false">
+            <i className="fa fa-user-circle fs-1 color-primary"/>
+            <span className="mx-3">Name Surname</span>
+          </a>
+          <ul className="dropdown-menu text-small" aria-labelledby="account-dropdown">
+            <li><a className="dropdown-item" href="#">Sign out</a></li>
+          </ul>
+        </div>
+
       </div>
     </div>
   )
--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -4,6 +4,7 @@ import App from './components/app/App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.min.js"
 
 ReactDOM.render(
   <BrowserRouter>
