31953ae9e03bea47451f31b33450ad8630597c4f
Ruben Danielyan <ruben.danielyan@disqo.com>
2022-03-12 22:47:41 +0400

    Home page

--- ui/src/components/home/Home.tsx
+++ ui/src/components/home/Home.tsx
@@ -1,11 +1,23 @@
 import React from "react";
 import Header from "../header/Header";
+import "./Home.css"
 
 function Home() {
   return (
     <>
-      <Header />
-      <div>Home</div>
+      <div className="home-wrapper">
+        <Header />
+
+        <div className="d-flex justify-content-center">
+          <div className="upload-wrapper d-flex justify-content-center align-items-center flex-column">
+            <p className="text-center fs-2">Upload PDF file</p>
+            <button className="btn form-btn mt-4">
+              <i className="fa fa-upload"/> Add Dashlet
+            </button>
+          </div>
+        </div>
+
+      </div>
     </>
   )
 }
